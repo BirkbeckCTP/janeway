508c5c5f13c19c19c2eedb44be35a27f0c654af1
Joseph Muller <j.muller@bbk.ac.uk>
2024-06-26 15:07:11 +0100

    Revert "Deprecate old user views, urls, and templates #4185"
    This reverts commit 1ac4f2cd0590c486fefd3767a73da1ef6ad3535f.

--- src/core/views.py
+++ src/core/views.py
@@ -1064,12 +1064,10 @@ def edit_plugin_settings_groups(request, plugin, setting_group_name, journal=Non
 @editor_user_required
 def roles(request):
     """
-    Deprecated.
     Displays a list of the journal roles
     :param request: HttpRequest object
     :return: HttpResponse object
     """
-    raise DeprecationWarning('Use JournalUsers instead')
     template = 'core/manager/roles/roles.html'
 
     roles = models.Role.objects.all().exclude(slug='reader')
@@ -1162,12 +1160,10 @@ def role_action(request, slug, user_id, action):
 @editor_user_required
 def users(request):
     """
-    Deprecated
     Displays a list of users, allows multiple users to be added to a role.
     :param request: HttpRequest object
     :return: HttpResponse object
     """
-    raise DeprecationWarning('Use JournalUsers instead')
     if request.POST:
         users = request.POST.getlist('users')
         role = request.POST.get('role')
@@ -1284,12 +1280,10 @@ def user_edit(request, user_id):
 @staff_member_required
 def inactive_users(request):
     """
-    Deprecated.
     Displays a list of inactive user accounts.
     :param request: HttpRequest object
     :return: HttpResponse object
     """
-    raise DeprecationWarning('Use AllUsers instead')
     user_list = models.Account.objects.filter(is_active=False)
 
     template = 'core/manager/users/inactive.html'
@@ -1361,10 +1355,6 @@ def user_history(request, user_id):
 
 @editor_user_required
 def enrol_users(request):
-    """
-    Deprecated
-    """
-    raise DeprecationWarning('Use AllUsers instead')
     user_search = []
     first_name = request.GET.get('first_name', '')
     last_name = request.GET.get('last_name', '')
